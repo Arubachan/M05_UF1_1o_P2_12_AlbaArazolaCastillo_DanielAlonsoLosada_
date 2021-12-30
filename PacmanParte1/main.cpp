4089b635b7e223e73ab396da3a7e4a7eaf0d43e6
Alba Arazola Castillo <albaarazolacastillo@gmail.com>
2021-12-30 20:57:54 +0100

    Power Up

--- PacmanParte1/main.cpp
+++ PacmanParte1/main.cpp
@@ -140,10 +140,37 @@ void Logic()
             player_points++;
             pacman_map.SetTile(player_x_new, player_y_new, Map::MAP_TILES::MAP_EMPTY);
             break;
+
+        case Map::MAP_TILES::MAP_POWERUP:
+            player_points += 25;
+            for (size_t i = 0; i < enemigos.size(); i++)
+            {
+                enemigos[i].PowerUpPicked();
+            }
+            pacman_map.SetTile(player_x_new, player_y_new, Map::MAP_TILES::MAP_EMPTY);
+            break;
+
         }
 
         player_y = player_y_new;
         player_x = player_x_new;
+
+
+        for (size_t i = 0; i < enemigos.size(); i++)
+        {
+            Enemy::ENEMY_STATE enemystate = enemigos[i].Update(&pacman_map, { (short)player_x , (short)player_y });
+            switch (enemystate)
+            {
+            case Enemy::ENEMY_KILLED:
+                player_points += 50;
+                break;
+            case Enemy::ENEMY_DEAD:
+                player_x = pacman_map.spawn_player.X;
+                player_y = pacman_map.spawn_player.Y;
+                break;
+            }
+        }
+
         if (pacman_map.points <= 0)
         {
             win = true;
@@ -158,6 +185,13 @@ void Draw()
     ConsoleUtils::Console_SetPos(player_x, player_y);
     ConsoleUtils::Console_SetColor(ConsoleUtils::CONSOLE_COLOR::DARK_YELLOW);
     std::cout << player_char;
+
+
+    for (size_t i = 0; i < enemigos.size(); i++)
+    {
+        enemigos[i].Draw();
+    }
+
     ConsoleUtils::Console_ClearCharacter({ 0,(short)pacman_map.Height });
     ConsoleUtils::Console_SetColor(ConsoleUtils::CONSOLE_COLOR::CYAN);
     std::cout << "Puntuacion actual: " << player_points << " Puntuacion pendiente: " << pacman_map.points << std::endl;
@@ -166,4 +200,8 @@ void Draw()
         ConsoleUtils::Console_SetColor(ConsoleUtils::CONSOLE_COLOR::GREEN);
         std::cout << "Has ganado!" << std::endl;
     }
+    std::cout << "Fotogramas: " << TimeManager::getInstance().frameCount << std::endl;
+    std::cout << "Time: " << TimeManager::getInstance().time << std::endl;
+    std::cout << "DeltaTime: " << TimeManager::getInstance().deltaTime << std::endl;
+    TimeManager::getInstance().NextFrame();
 }
